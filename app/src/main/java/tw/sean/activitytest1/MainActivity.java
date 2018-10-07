09c7db1f8630925b148f514f4c60774fc759e75a
Seanhhe <seanhhe@gmail.com>
2018-10-07 11:52:52 +0800

    20181007_AS_ActivityTest1_v2

--- app/src/main/java/tw/sean/activitytest1/MainActivity.java
+++ app/src/main/java/tw/sean/activitytest1/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private long lastBackTime = 0;
 
     public MainActivity() {
         Log.d( "brad",  "MainActivity()");
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         //從哪裡去哪裡.class，在手機中任一class在記憶體中的位址只有一個
         Intent intent = new Intent(this, Page2Activity.class);
         startActivity(intent);
+        super.finish();//加上此行，切換頁面時，此舊頁面會從記憶體堆疊中被殺掉。新頁面會new出來
     }
 
 
@@ -65,5 +68,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d("brad","onDestory");
+    }
+    //呼叫finish()方法，自殺，生命週期走的路線與按下"back"相同，onPause,onStop,onDestory.
+    //非正規用法
+    public void die(View view) {
+        finish();
+    }
+
+    //方法二
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();//會去呼叫super.finish()，若註解掉，則系統back鍵失效，只能由設計的結束鍵finish()
+        Log.d("brad","onBackPressed()");
+    }
+
+    //方法一
+    @Override
+    public void finish() {
+        //Log.d("brad","before super.finish()");//收尾動作要寫在super.finish()之前
+        //super.finish();//真正的死點在此，觸發super.finish
+        //Log.d("brad","after super.finish()");
+
+        long nowTime = System.currentTimeMillis();
+
+        if (nowTime - lastBackTime <= 3*1000 ){
+            super.finish();
+        }else {
+            Toast.makeText(this, "在按一次就離開", Toast.LENGTH_SHORT).show();
+            lastBackTime = nowTime;
+        }
+
     }
 }
--- app/src/main/java/tw/sean/activitytest1/Page2Activity.java
+++ app/src/main/java/tw/sean/activitytest1/Page2Activity.java
@@ -1,7 +1,9 @@
 package tw.sean.activitytest1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Page2Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Page2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+    }
+
+    public void test2(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();//加上此行，切換頁面時，此舊頁面會從記憶體堆疊中被殺掉。新頁面會new出來
     }
 }
